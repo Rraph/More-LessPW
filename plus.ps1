e275b9038510701c2ffb511477b94431bbf4a648
Raphael BEGHIN <raphael.beghin@gifi.fr>
2019-06-03 11:19:30 +0200

    ajout du script

---
+++ plus.ps1
@@ -0,0 +1,16 @@
+Write-Host "############## BIENVENUE DANS LE JEU DU PLUS OU MOINS ####################" -f "green"
+
+$nombre = Get-Random -Maximum "100" -Minimum "0"
+
+do {
+    $choix = Read-Host "Entrez un nombre"
+    if ($choix -gt $nombre) {
+        Write-Host "c'est moins" -f "red"
+    }
+    elseif ($choix -lt $nombre) {
+        Write-Host "c'est plus" -f "red"
+    }
+    else {
+        Write-Host "You win !" -f "green"
+    }
+} while ($choix -ne $nombre)
